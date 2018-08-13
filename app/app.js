fc6d5d8475d68baf7e45de7a51241fa7539356fc
nad <snadav54@gmail.com>
2018-08-13 13:54:32 -0700

    fixed app.js to work with minification

--- app/app.js
+++ app/app.js
@@ -1,24 +1,24 @@
 var routerApp = angular.module('routerApp', ['ui.router','ui.grid','ui.bootstrap']);
 
 
-routerApp.controller('expensesListCtrl',function ($http,gridService) {
-    var vm = this;
-    vm.recievedData = false;
-    vm.searchInput;
-    vm.gridOptions = gridService.getOption();
-    //vm.gridOptions.columnDefs = [{field:"name"},{field:"amount"},{field:"type"},{field:"time",type:'date'}];
+routerApp.controller('expensesListCtrl',["$http", "gridService", function ($http,gridService) {
+  var vm = this;
+  vm.recievedData = false;
+  vm.searchInput;
+  vm.gridOptions = gridService.getOption();
+  //vm.gridOptions.columnDefs = [{field:"name"},{field:"amount"},{field:"type"},{field:"time",type:'date'}];
 
-    $http.get("http://localhost:8080/expenses").then(function(response){
+  $http.get("http://localhost:8080/expenses").then(function(response){
 
-      gridService.setData(response.data.expenses);
-      gridService.setUnfilteredData(response.data.expenses);
-      vm.recievedData = true;
-    }).catch(function (error) {
-        console.log(error);
-    });
-});
+    gridService.setData(response.data.expenses);
+    gridService.setUnfilteredData(response.data.expenses);
+    vm.recievedData = true;
+  }).catch(function (error) {
+    console.log(error);
+  });
+}]);
 
-routerApp.factory('gridService',function () {
+routerApp.factory('gridService',[function () {
 
 
   var gridServiceInstance = {};
@@ -48,71 +48,71 @@ routerApp.factory('gridService',function () {
 
   return gridServiceInstance;
 
-})
+}])
 
-routerApp.controller('mainPageNavCtrl',function ($scope,$uibModal,$http,gridService) {
+routerApp.controller('mainPageNavCtrl',["$scope", "$uibModal", "$http", "gridService", function ($scope,$uibModal,$http,gridService) {
 
-    var vm = this;
-    $scope.check = "check str";
+  var vm = this;
+  $scope.check = "check str";
 
 
-    $scope.addPath = function(){
-        console.log("add path");
-        $uibModal.open({
-            templateUrl: 'addPathModal.html',
-            controller: function ($scope, $uibModalInstance) {
+  $scope.addPath = function(){
+    console.log("add path");
+    $uibModal.open({
+      templateUrl: 'addPathModal.html',
+      controller: ["$scope", "$uibModalInstance", function ($scope, $uibModalInstance) {
 
-                $scope.addExpenseForm = {};
+        $scope.addExpenseForm = {};
 
-                $scope.ok = function () {
-                    $http.post("http://localhost:8080/expenses",$scope.addExpenseForm).then(function(response){
-                        console.log(response);
-                    })
-                    $uibModalInstance.close();
-                };
+        $scope.ok = function () {
+          $http.post("http://localhost:8080/expenses",$scope.addExpenseForm).then(function(response){
+            console.log(response);
+          })
+          $uibModalInstance.close();
+        };
 
-                $scope.cancel = function () {
-                    $uibModalInstance.dismiss('cancel');
-                };
-            }
-        })
+        $scope.cancel = function () {
+          $uibModalInstance.dismiss('cancel');
+        };
+      }]
+    })
+  }
+  $scope.searchText = function () {
+    console.log("inside search text");
+
+    if($scope.searchInputText == null || $scope.searchInputText.length == 0)
+    {
+      gridService.setData(gridService.getUnfilteredData());
+      return;
     }
-    $scope.searchText = function () {
-      console.log("inside search text");
 
-      if($scope.searchInputText == null || $scope.searchInputText.length == 0)
-      {
-        gridService.setData(gridService.getUnfilteredData());
-        return;
+    var filteredData = gridService.getUnfilteredData().filter(function (element) {
+      if ( (element.name.indexOf($scope.searchInputText) != -1) || element.type.indexOf($scope.searchInputText) != -1) {
+        return true;
       }
+      return false;
+    });
+    gridService.setData(filteredData);
+  }
 
-      var filteredData = gridService.getUnfilteredData().filter(function (element) {
-        if ( (element.name.indexOf($scope.searchInputText) != -1) || element.type.indexOf($scope.searchInputText) != -1) {
-          return true;
-        }
-        return false;
-      });
-      gridService.setData(filteredData);
-    }
-
-})
+}])
 
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+routerApp.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
-    $stateProvider
+  $urlRouterProvider.otherwise('/home');
+  $stateProvider
 
-    // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-            templateUrl: 'expensesList.html',
-            controller: 'expensesListCtrl as vm'
-        })
+  // HOME STATES AND NESTED VIEWS ========================================
+    .state('home', {
+      url: '/home',
+      templateUrl: 'expensesList.html',
+      controller: 'expensesListCtrl as vm'
+    })
 
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            // we'll get to this in a bit
-        });
+    // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+    .state('about', {
+      // we'll get to this in a bit
+    });
 
-});
+}]);
